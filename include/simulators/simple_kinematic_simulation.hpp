2928e8e4148956e0f736e48ab55861580f746637
Santiago Iregui <santiago.iregui@kuleuven.be>
2025-08-22 18:49:06 +0200

    Simulator working with boost triple buffer. Still need to organize code and make it more generic

--- include/simulators/simple_kinematic_simulation.hpp
+++ include/simulators/simple_kinematic_simulation.hpp
@@ -5,6 +5,8 @@
 
 #include "robot_interfacing_utils/robotsimulator.hpp"
 #include "robot_interfacing_utils/controlmodes_enum.hpp"
+#include "robot_interfacing_utils/robot_data_structures.hpp"
+
 
 
 namespace etasl {
@@ -15,6 +17,7 @@ class simple_kinematic_simulation : public RobotSimulator {
 
 
     private:
+       static constexpr int NUM_JOINTS = 6; //TODO: for now assume 6 joints
         
         // FeedbackMsg* feedback_ptr; Defined in super class RobotSimulator at header file robotsimulator.hpp
         // SetpointMsg* setpoint_ptr; Defined in super class RobotSimulator at header file robotsimulator.hpp
@@ -23,8 +26,37 @@ class simple_kinematic_simulation : public RobotSimulator {
         double periodicity;
         ControlMode::ControlMode control_mode;
 
-        std::vector<double> initial_joints;
-        std::vector<double> joint_pos;
+        std::vector<float> initial_joints;
+        robotdrivers::FixedJointDataField<NUM_JOINTS> setpoint_joint_vel;
+        robotdrivers::FixedJointDataField<NUM_JOINTS> joint_pos;
+
+        typedef boost::lockfree::spsc_value< robotdrivers::FixedJointDataField<NUM_JOINTS> > triple_buffer_joint_type;
+        typedef boost::lockfree::spsc_value< robotdrivers::Vector3Field > triple_buffer_vector3_type;
+        typedef boost::lockfree::spsc_value< robotdrivers::QuaternionField2 > triple_buffer_quaternion_type;
+        typedef boost::lockfree::spsc_value< robotdrivers::ScrewField2 > triple_buffer_screw_type;
+
+
+        
+        struct FeedbackTripleBuffers {
+            std::shared_ptr<triple_buffer_joint_type> joint_pos;
+            std::shared_ptr<triple_buffer_joint_type> joint_vel;
+            std::shared_ptr<triple_buffer_joint_type> joint_torque;
+            std::shared_ptr<triple_buffer_joint_type> joint_current;
+            std::shared_ptr<triple_buffer_vector3_type> cartesian_pos;
+            std::shared_ptr<triple_buffer_quaternion_type> cartesian_quat;
+            std::shared_ptr<triple_buffer_screw_type> cartesian_twist;
+            std::shared_ptr<triple_buffer_screw_type> cartesian_wrench;
+            std::shared_ptr<triple_buffer_vector3_type> base_pos;
+            std::shared_ptr<triple_buffer_quaternion_type> base_quat;
+            std::shared_ptr<triple_buffer_screw_type> base_twist;
+        } feedback_triple_buffers_ptrs;  
+
+        struct SetpointTripleBuffers {
+            std::shared_ptr<triple_buffer_joint_type> joint_vel;
+        } setpoint_triple_buffers_ptrs;  
+
+        bool readSetpointJointVelocity(robotdrivers::FixedJointDataField<NUM_JOINTS>& sp ) noexcept;
+        void writeFeedbackJointPosition(const robotdrivers::FixedJointDataField<NUM_JOINTS>& fb ) noexcept;
         
 
     public:
@@ -56,9 +88,18 @@ class simple_kinematic_simulation : public RobotSimulator {
         // virtual void* getSetpointJointVelocityBufferPtr() override;
         // virtual void* getJointPositionBufferPtr() override;
 
+        void writeSetpointJointVelocity(const std::vector<float>& sp ) noexcept override;
+        bool readFeedbackJointPosition(std::vector<float>& fb ) noexcept override;
+
+
         // virtual const std::string& getName() const override;
 
         virtual ~simple_kinematic_simulation();
+
+
+
+
+
 };
 
 } // namespace etasl
--- src/simulators/simple_kinematic_simulation.cpp
+++ src/simulators/simple_kinematic_simulation.cpp
@@ -21,47 +21,75 @@ void simple_kinematic_simulation::construct(std::string robot_name,
 
     periodicity = jsonchecker->asDouble(config, "periodicity");
 
-    std::vector<double> init_joints;
+    std::vector<float> init_joints;
     for (auto n : jsonchecker->asArray(config, "initial_joints")) {
         init_joints.push_back(jsonchecker->asDouble(n, ""));
     }
 
     initial_joints = init_joints;
-    joint_pos = initial_joints;
+
+    assert(initial_joints.size() == joint_pos.data.size());
+
+    // joint_pos.data = initial_joints;
+    std::copy(initial_joints.begin(), initial_joints.end(), joint_pos.data.begin());
+
+    setpoint_joint_vel.data.fill( 0.0); //Initialize setpoint joint velocities to zero
 
     feedback_ptr = fb; //defined in RobotDriver super class.
     setpoint_ptr = sp; //defined in RobotDriver super class.
     name = robot_name; //defined in RobotDriver super class.
     std::cout << "Constructed object of simple_kinematic_simulation class with name: " << name << std::endl;
 
+    // struct TripleBuffers {
+    //     std::shared_ptr<triple_buffer_joint_type> joint_vel;
+    //     std::shared_ptr<triple_buffer_joint_type> joint_torque;
+    //     std::shared_ptr<triple_buffer_joint_type> joint_current;
+    //     std::shared_ptr<triple_buffer_vector3_type> cartesian_pos;
+    //     std::shared_ptr<triple_buffer_quaternion_type> cartesian_quat;
+    //     std::shared_ptr<triple_buffer_screw_type> cartesian_twist;
+    //     std::shared_ptr<triple_buffer_screw_type> cartesian_wrench;
+    //     std::shared_ptr<triple_buffer_vector3_type> base_pos;
+    //     std::shared_ptr<triple_buffer_quaternion_type> base_quat;
+    //     std::shared_ptr<triple_buffer_screw_type> base_twist;
+    // } triple_buffers_ptrs;  
+
+
+    feedback_triple_buffers_ptrs.joint_pos = std::make_shared<triple_buffer_joint_type>(joint_pos);
+    setpoint_triple_buffers_ptrs.joint_vel = std::make_shared<triple_buffer_joint_type>(setpoint_joint_vel);
+
+    // using T = std::vector<double>;
+    // boost::lockfree::spsc_value<T, boost::lockfree::allow_multiple_reads<false>> ch;
+    // boost::lockfree::spsc_value< robotdrivers::FixedJointDataField<NUM_JOINTS> > triple_buffer_joint_type;
+
 }
 
 bool simple_kinematic_simulation::initialize()
 {
-    joint_pos = initial_joints;
+    // joint_pos.data = initial_joints;
+    std::copy(initial_joints.begin(), initial_joints.end(), joint_pos.data.begin());
 
-    feedback_ptr->mtx.lock();
-    // setpoint_ptr->mtx.lock();
+    feedback_triple_buffers_ptrs.joint_pos->write(joint_pos);
 
-    feedback_ptr->joint.pos.data = joint_pos;
-    feedback_ptr->joint.pos.is_available = true;
+    // feedback_ptr->mtx.lock();
+
+    // feedback_ptr->joint.pos.data = joint_pos;
+    // feedback_ptr->joint.pos.is_available = true;
     
-    //Uncomment to simulate the sensor data
-    // feedback_ptr->joint.vel.is_available = true;
-    // feedback_ptr->joint.torque.is_available = true;
-    // feedback_ptr->joint.current.is_available = true;
+    // //Uncomment to simulate the sensor data
+    // // feedback_ptr->joint.vel.is_available = true;
+    // // feedback_ptr->joint.torque.is_available = true;
+    // // feedback_ptr->joint.current.is_available = true;
 
-    // feedback_ptr->cartesian.pos.is_available = true;
-    // feedback_ptr->cartesian.quat.is_available = true;
-    // feedback_ptr->cartesian.twist.is_available = true;
-    // feedback_ptr->cartesian.wrench.is_available = true;
+    // // feedback_ptr->cartesian.pos.is_available = true;
+    // // feedback_ptr->cartesian.quat.is_available = true;
+    // // feedback_ptr->cartesian.twist.is_available = true;
+    // // feedback_ptr->cartesian.wrench.is_available = true;
 
-    // feedback_ptr->base.pos.is_available = true;
-    // feedback_ptr->base.quat.is_available = true;
-    // feedback_ptr->base.twist.is_available = true;
+    // // feedback_ptr->base.pos.is_available = true;
+    // // feedback_ptr->base.quat.is_available = true;
+    // // feedback_ptr->base.twist.is_available = true;
 
-    // setpoint_ptr->mtx.unlock();
-    feedback_ptr->mtx.unlock();
+    // feedback_ptr->mtx.unlock();
 
     return true;
 }
@@ -69,85 +97,86 @@ bool simple_kinematic_simulation::initialize()
 
 void simple_kinematic_simulation::update(volatile std::atomic<bool>& stopFlag)
 {
-    feedback_ptr->mtx.lock();
-    // setpoint_ptr->mtx.lock();
+    // feedback_ptr->mtx.lock();
 
-    assert(feedback_ptr->joint.pos.data.size() == setpoint_ptr->velocity.data.size());
+    
+    // setpoint_triple_buffers_ptrs.joint_vel->read(setpoint_joint_vel);
+    readSetpointJointVelocity(setpoint_joint_vel);
 
-    for (unsigned int i=0; i<feedback_ptr->joint.pos.data.size(); ++i) {
-        joint_pos[i] += setpoint_ptr->velocity.data[i]*periodicity; //simple integration
-        // joint_pos[i] += setpoint_ptr->velocity.data[i]*0.0000005; //simple integration
-        // joint_pos[i] += 0.00000001; //test
-        feedback_ptr->joint.pos.data[i] = joint_pos[i];
+    assert(joint_pos.data.size() == setpoint_joint_vel.data.size());
+
+    for (unsigned int i=0; i<setpoint_joint_vel.data.size(); ++i) {
+        joint_pos.data[i] += setpoint_joint_vel.data[i]*periodicity; //simple integration
     }
 
-    //Uncomment to simulate the sensor data
-    // feedback_ptr->joint.vel.data[0] = 10.1;
-    // feedback_ptr->joint.vel.data[1] = 10.2;
-    // feedback_ptr->joint.vel.data[2] = 10.3;
-    // feedback_ptr->joint.vel.data[3] = 10.4;
+    feedback_triple_buffers_ptrs.joint_pos->write(joint_pos);
+    // writeFeedbackJointPosition(joint_pos);
 
-    // feedback_ptr->joint.torque.data[0] = 20.1;
-    // feedback_ptr->joint.torque.data[1] = 20.2;
-    // feedback_ptr->joint.torque.data[2] = 20.3;
-    // feedback_ptr->joint.torque.data[3] = 20.4;
+    // //Uncomment to simulate the sensor data
+    // // feedback_ptr->joint.vel.data[0] = 10.1;
+    // // feedback_ptr->joint.vel.data[1] = 10.2;
+    // // feedback_ptr->joint.vel.data[2] = 10.3;
+    // // feedback_ptr->joint.vel.data[3] = 10.4;
 
-    // feedback_ptr->joint.current.data[0] = 30.1;
-    // feedback_ptr->joint.current.data[1] = 30.2;
-    // feedback_ptr->joint.current.data[2] = 30.3;
-    // feedback_ptr->joint.current.data[3] = 30.4;
+    // // feedback_ptr->joint.torque.data[0] = 20.1;
+    // // feedback_ptr->joint.torque.data[1] = 20.2;
+    // // feedback_ptr->joint.torque.data[2] = 20.3;
+    // // feedback_ptr->joint.torque.data[3] = 20.4;
 
-    // feedback_ptr->cartesian.pos.x = 1.1;
-    // feedback_ptr->cartesian.pos.y = 1.2;
-    // feedback_ptr->cartesian.pos.z = 1.3;
+    // // feedback_ptr->joint.current.data[0] = 30.1;
+    // // feedback_ptr->joint.current.data[1] = 30.2;
+    // // feedback_ptr->joint.current.data[2] = 30.3;
+    // // feedback_ptr->joint.current.data[3] = 30.4;
 
-    // feedback_ptr->cartesian.quat.qx = 2.1;
-    // feedback_ptr->cartesian.quat.qy = 2.2;
-    // feedback_ptr->cartesian.quat.qz = 2.3;
-    // feedback_ptr->cartesian.quat.qw = 2.3;
+    // // feedback_ptr->cartesian.pos.x = 1.1;
+    // // feedback_ptr->cartesian.pos.y = 1.2;
+    // // feedback_ptr->cartesian.pos.z = 1.3;
 
-    // feedback_ptr->cartesian.twist.linear.x = 3.1;
-    // feedback_ptr->cartesian.twist.linear.y = 3.2;
-    // feedback_ptr->cartesian.twist.linear.z = 3.3;
-    // feedback_ptr->cartesian.twist.angular.x = 3.4;
-    // feedback_ptr->cartesian.twist.angular.y = 3.5;
-    // feedback_ptr->cartesian.twist.angular.z = 3.6;
+    // // feedback_ptr->cartesian.quat.qx = 2.1;
+    // // feedback_ptr->cartesian.quat.qy = 2.2;
+    // // feedback_ptr->cartesian.quat.qz = 2.3;
+    // // feedback_ptr->cartesian.quat.qw = 2.3;
 
-    // feedback_ptr->cartesian.wrench.linear.x = 0;
-    // feedback_ptr->cartesian.wrench.linear.y = 0;
-    // feedback_ptr->cartesian.wrench.linear.z = 0;
-    // feedback_ptr->cartesian.wrench.angular.x = 0;
-    // feedback_ptr->cartesian.wrench.angular.y = 0;
-    // feedback_ptr->cartesian.wrench.angular.z = 0;
+    // // feedback_ptr->cartesian.twist.linear.x = 3.1;
+    // // feedback_ptr->cartesian.twist.linear.y = 3.2;
+    // // feedback_ptr->cartesian.twist.linear.z = 3.3;
+    // // feedback_ptr->cartesian.twist.angular.x = 3.4;
+    // // feedback_ptr->cartesian.twist.angular.y = 3.5;
+    // // feedback_ptr->cartesian.twist.angular.z = 3.6;
+
+    // // feedback_ptr->cartesian.wrench.linear.x = 0;
+    // // feedback_ptr->cartesian.wrench.linear.y = 0;
+    // // feedback_ptr->cartesian.wrench.linear.z = 0;
+    // // feedback_ptr->cartesian.wrench.angular.x = 0;
+    // // feedback_ptr->cartesian.wrench.angular.y = 0;
+    // // feedback_ptr->cartesian.wrench.angular.z = 0;
 
 
-    // feedback_ptr->base.pos.x = 1.1;
-    // feedback_ptr->base.pos.y = 1.2;
-    // feedback_ptr->base.pos.z = 1.3;
+    // // feedback_ptr->base.pos.x = 1.1;
+    // // feedback_ptr->base.pos.y = 1.2;
+    // // feedback_ptr->base.pos.z = 1.3;
 
-    // feedback_ptr->base.quat.qx = 2.1;
-    // feedback_ptr->base.quat.qy = 2.2;
-    // feedback_ptr->base.quat.qz = 2.3;
-    // feedback_ptr->base.quat.qw = 2.3;
+    // // feedback_ptr->base.quat.qx = 2.1;
+    // // feedback_ptr->base.quat.qy = 2.2;
+    // // feedback_ptr->base.quat.qz = 2.3;
+    // // feedback_ptr->base.quat.qw = 2.3;
 
-    // feedback_ptr->base.twist.linear.x = 3.1;
-    // feedback_ptr->base.twist.linear.y = 3.2;
-    // feedback_ptr->base.twist.linear.z = 3.3;
-    // feedback_ptr->base.twist.angular.x = 3.4;
-    // feedback_ptr->base.twist.angular.y = 3.5;
-    // feedback_ptr->base.twist.angular.z = 3.6;
+    // // feedback_ptr->base.twist.linear.x = 3.1;
+    // // feedback_ptr->base.twist.linear.y = 3.2;
+    // // feedback_ptr->base.twist.linear.z = 3.3;
+    // // feedback_ptr->base.twist.angular.x = 3.4;
+    // // feedback_ptr->base.twist.angular.y = 3.5;
+    // // feedback_ptr->base.twist.angular.z = 3.6;
     
 
-    setpoint_ptr->velocity.fs = etasl::OldData;
-    // std::cout << "vel val:" << setpoint_ptr->velocity.data[0] << " , " << setpoint_ptr->velocity.data[1] << " , "<< setpoint_ptr->velocity.data[2] << std::endl;
+    // setpoint_ptr->velocity.fs = etasl::OldData;
+    // // std::cout << "vel val:" << setpoint_ptr->velocity.data[0] << " , " << setpoint_ptr->velocity.data[1] << " , "<< setpoint_ptr->velocity.data[2] << std::endl;
+    // // std::cout << "Driver update has set all pos values to " << feedback_ptr->joint.pos.data[0] << std::endl;
+    // // std::cout << "Driver update has set all pos values to " << this->periodicity << std::endl;
+    // // std::cout << "Driver update has set all pos values to " << getName() << std::endl;
 
 
-    // std::cout << "Driver update has set all pos values to " << feedback_ptr->joint.pos.data[0] << std::endl;
-    // std::cout << "Driver update has set all pos values to " << this->periodicity << std::endl;
-    // std::cout << "Driver update has set all pos values to " << getName() << std::endl;
-
-    // setpoint_ptr->mtx.unlock();
-    feedback_ptr->mtx.unlock();
+    // feedback_ptr->mtx.unlock();
 
 
 }
@@ -179,11 +208,82 @@ void simple_kinematic_simulation::finalize() {
 
 }
 
+// -----------Functions for thread communicating with this driver -------------- 
+
+void simple_kinematic_simulation::writeSetpointJointVelocity(const std::vector<float>& sp ) noexcept{
+
+    // std::cout << "1 im writing joint velocity setpoints" << std::endl;
+
+    static robotdrivers::FixedJointDataField<NUM_JOINTS> buff{};
+
+    assert(sp.size() == buff.data.size());
+    
+    for (unsigned int i=0; i<buff.data.size(); ++i) {
+        buff.data[i] = sp[i]; //copy
+    }
+    
+
+    #ifdef INCLUDE_TIMESTAMP_IN_DATA_STRUCTURE
+    buff.timestamp = std::chrono::steady_clock::now();
+    #endif
+
+    setpoint_triple_buffers_ptrs.joint_vel->write(buff);
+
+}
+
+bool simple_kinematic_simulation::readFeedbackJointPosition(std::vector<float>& fb ) noexcept{
+    // std::cout << "2 im reading joint position feedback" << std::endl;
+
+
+    static robotdrivers::FixedJointDataField<NUM_JOINTS> joint_local_buff;
+    bool ret = feedback_triple_buffers_ptrs.joint_pos->read(joint_local_buff);
+
+    assert(fb.size() == joint_local_buff.data.size());
+
+    for (unsigned int i=0; i<joint_local_buff.data.size(); ++i) {
+        fb[i] = joint_local_buff.data[i]; //copy
+    }
+
+    return ret;
+
+    // #ifdef INCLUDE_TIMESTAMP_IN_DATA_STRUCTURE
+    // buff.timestamp = std::chrono::steady_clock::now();
+    //TODO: benchmark
+    // #endif
+} 
+
+// -----------Functions for this driver -------------- 
+
+bool simple_kinematic_simulation::readSetpointJointVelocity(robotdrivers::FixedJointDataField<NUM_JOINTS>& sp ) noexcept{
+
+    // std::cout << "3 im reading joint velocity setpoints" << std::endl;
+    return setpoint_triple_buffers_ptrs.joint_vel->read(sp);
+
+    // #ifdef INCLUDE_TIMESTAMP_IN_DATA_STRUCTURE
+    // buff.timestamp = std::chrono::steady_clock::now();
+    //TODO: benchmark
+    // #endif
+} 
+
+void simple_kinematic_simulation::writeFeedbackJointPosition(const robotdrivers::FixedJointDataField<NUM_JOINTS>& fb ) noexcept{
+
+    // std::cout << "4 im writing joint position feedback" << std::endl;
+    feedback_triple_buffers_ptrs.joint_pos->write(fb);
+}
+
 
 
 simple_kinematic_simulation::~simple_kinematic_simulation() {
+    //delete buffers
+    // feedback_triple_buffers_ptrs.joint_pos.reset();
+    // setpoint_triple_buffers_ptrs.joint_vel.reset();
+    std::cout << "destructor called for simple_kinematic_simulation with name: " << name << std::endl;
 
 };
+
+
+// void* simple_kinematic_simulation::getSetpointJointVelocityBufferPtr(){return (void*)setpoint_triple_buffers_ptrs.joint_vel.get();};
+// void* simple_kinematic_simulation::getJointPositionBufferPtr(){return (void*)feedback_triple_buffers_ptrs.joint_pos.get();};
 
 
 
